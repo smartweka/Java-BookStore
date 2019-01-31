524b8eb548b14249bff47acc02c6d77a21835cc9
smarweka <weka@smartweka.eu>
2019-01-31 19:59:41 +0100

    Module 2

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 					deleteBook(request, response);
 					break;
+				case "/edit":
+					showEditForm(request, response);
+					break;
         		default:
 				   listBooks(request, response);
     	           break;
@@ -93,6 +96,14 @@ public class ControllerServlet extends HttpServlet {
 	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		dispatcher.forward(request, response);
+	}
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.valueOf(request.getParameter("id"));
+		Book book = bookDAO.getBook(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", book);
 		dispatcher.forward(request, response);
 	}
 
